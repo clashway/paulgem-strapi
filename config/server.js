96b3b40cb341113394ca0d09c300e254da7e6982
Cory Lashway <clashway@estee.com>
2021-10-28 12:42:44 -0400

    asdf

--- config/server.js
+++ config/server.js
@@ -1,3 +1,9 @@
 module.exports = ({ env }) => ({
-  url: env("HEROKU_URL"),
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e761bf18350b912e8eb1fc29c81db7a5'),
+    },
+  },
 });
