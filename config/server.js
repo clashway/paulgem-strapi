0a180f82230cfc8d5308a3f73cd13e7f72a92ff4
Cory Lashway <clashway@estee.com>
2021-10-28 12:24:31 -0400

    update server

--- config/server.js
+++ config/server.js
@@ -1,9 +1,3 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'e761bf18350b912e8eb1fc29c81db7a5'),
-    },
-  },
+  url: env("HEROKU_URL"),
 });
