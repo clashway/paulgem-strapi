8ea800944bc2f7cc1565d70b2ed6f8b1de17c1bd
Leonardo Maddio <leonardo.maddio@gmail.com>
2021-10-29 16:10:20 -0300

    use custom find for preview mode

--- api/product/controllers/product.js
+++ api/product/controllers/product.js
@@ -11,6 +11,36 @@ module.exports = {
     const { slug } = ctx.params;
 
     const entity = await strapi.services.product.findOne({ slug });
+
     return sanitizeEntity(entity, { model: strapi.models.product });
+  },
+  async find(ctx) {
+    if (ctx.query) {
+      const { status = 'published', ...restQuery } = ctx.query;
+
+      const isPublished = status === 'published' || status.includes('published');
+      const isDraft = status === 'draft' || status.includes('draft');
+
+      const ifBoth = isPublished && isDraft ? {} : null;
+
+      const isOnlyDraft = !ifBoth && isDraft ? { published_at_null: "t" } : {};
+
+      const isOnlyPublished = !ifBoth && isPublished ? { published_at_null: "f" } : {};
+
+      const queryParams = {
+        ...restQuery,
+        ...(ifBoth || {
+          ...isOnlyDraft,
+          ...isOnlyPublished
+        })
+      };
+
+
+      const entity = await strapi.query('product').find(queryParams);
+
+      return sanitizeEntity(entity, { model: strapi.models.product });
+    }
+
+    return [];
   },
 };
